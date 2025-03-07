4f992d3213597fbd4c181f9380aa63cfdcb70506
Sergey <karakoskinsergej@gmail.com>
2025-03-07 17:24:01 +0300

    feat/main - App: add routing

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,28 +1,52 @@
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Header from "../header/Header";
 import SchedulePage from "../../pages/schedule/SchedulePage";
-// import HistoryPage from "../../pages/history/HistoryPage";
-// import CancelModal from "../modal/CancelModal";
+import HistoryPage from "../../pages/history/HistoryPage";
+import PageNotFound from "../../pages/404/404";
 
 import AppointmentContextProvider from "../../context/appointments/AppointmentsContext";
-
-
-
 import "./app.scss";
+
+// формируем пути в router, children - то что рендерится вместо outlet компонента в компоненте Root
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Root/>,
+		errorElement: <PageNotFound/>,
+		children: [
+			{
+				path: '/',
+				element: <SchedulePage/>
+			},
+			{
+				path: '/schedule',
+				element: <SchedulePage/>
+			},
+			{
+				path: '/history',
+				element: <HistoryPage/>
+			}
+		]
+	}
+]);
 
 
 
 function App() {
+	return <RouterProvider router={router}/>
+}
+
+
+// компонент - основная структура, которая не изменяется
+function Root() {
 	return (
 		<main className="board">
 			<Header />
 			<AppointmentContextProvider>
-				<SchedulePage />
+				<Outlet />
 			</AppointmentContextProvider>
-			
-			{/* <HistoryPage /> */}
-			{/* <CancelModal /> */}
 		</main>
-	);
+	)
 }
 
 export default App;
